e94d8ef601a0094a6b7f4f6dcb6a1613d147773c
sizuchok <sizuchok0@gmail.com>
2024-02-07 02:18:10 +0200

    feat: Implement account options dropdown-menu in navbar

--- src/common/components/layouts/primary-layout.tsx
+++ src/common/components/layouts/primary-layout.tsx
@@ -1,12 +1,30 @@
-import { BookmarkFilledIcon } from '@radix-ui/react-icons'
+import { BookmarkFilledIcon, UpdateIcon } from '@radix-ui/react-icons'
 import { Link, Outlet } from 'react-router-dom'
 import { useShallow } from 'zustand/react/shallow'
+import { useSignOut } from '../../../auth/hooks/sign-out.book'
 import { useAuthStore } from '../../../auth/store/auth-store'
 import { Avatar, AvatarFallback } from '../../../components/ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '../../../components/ui/dropdown-menu'
 import { ROUTER } from '../../const/router-keys.const'
 
 const PrimaryLayout = () => {
   const { user } = useAuthStore(useShallow(({ user }) => ({ user })))
+  const { mutate, isPending } = useSignOut()
+
+  if (isPending) {
+    return (
+      <div className="w-screen h-screen flex">
+        <UpdateIcon className="m-auto animate-spin w-8 h-9" />
+      </div>
+    )
+  }
 
   return (
     <div className="w-screen h-screen flex flex-col">
@@ -19,9 +37,18 @@ const PrimaryLayout = () => {
         </div>
 
         <div>
-          <Avatar>
-            <AvatarFallback>{user?.name.substring(0, 2) ?? '?'}</AvatarFallback>
-          </Avatar>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Avatar className="cursor-pointer">
+                <AvatarFallback>{user?.name.substring(0, 2) ?? '?'}</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>{user?.name ?? 'Acccount'}</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={() => mutate()}>LogOut</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
 
--- src/pages/dashboard/dashboard-page.tsx
+++ src/pages/dashboard/dashboard-page.tsx
@@ -1,26 +1,13 @@
-import { UpdateIcon } from '@radix-ui/react-icons'
 import { Link } from 'react-router-dom'
-import { useSignOut } from '../../auth/hooks/sign-out.book'
 import { ROUTER } from '../../common/const/router-keys.const'
 import { Button } from '../../components/ui/button'
 
 const DashboardPage = () => {
-  const { mutate, isPending } = useSignOut()
-
-  if (isPending) {
-    return (
-      <div className="w-screen h-screen flex">
-        <UpdateIcon className="m-auto animate-spin w-8 h-9" />
-      </div>
-    )
-  }
-
   return (
     <div className="space-x-4">
       <Button asChild>
         <Link to={ROUTER.DASHBOARD.BIKES}>Bikes</Link>
       </Button>
-      <Button onClick={() => mutate()}>Log out</Button>
     </div>
   )
 }
